0ac1045785c68bc2c1ccc80f2ddcb513c0db8f30
Arkadiusz Pasek <thevik444@gmail.com>
2020-05-07 11:48:54 +0200

    Changed default sound

--- src/reducers/audioReducer.js
+++ src/reducers/audioReducer.js
@@ -1,7 +1,7 @@
 import { SOUND_CHANGE } from '../actions/types';
 
-// const INITIAL_AUDIO = new Audio('../../res/ding.mp3');
-const INITIAL_AUDIO = new Audio('https://raw.githubusercontent.com/arkadiuszpasek/Online-Youtube-Timer/master/res/w3QuestSound.mp3');
+const INITIAL_AUDIO = new Audio('https://raw.githubusercontent.com/arkadiuszpasek/simple-timer/master/res/ding.mp3');
+// const INITIAL_AUDIO = new Audio('https://raw.githubusercontent.com/arkadiuszpasek/Online-Youtube-Timer/master/res/w3QuestSound.mp3');
 
 export default (state = INITIAL_AUDIO, action) => {
   switch (action.type) {
